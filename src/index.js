50a3d9910b9cda62fb69a99bf23dc7f3d10b9bd7
Akua1039 <fosua27@gmail.com>
2024-12-11 11:48:10 +0000

    added all

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@
 
     const num1 = document.getElementById('num1');
     num1.innerHTML = 'I know now what the DOM is 🙌';
-    num1.style.backgroundColor = ("grey"); // Light gray
+    num1.style.backgroundColor = ("grey");
     num1.style.color = ("black");
 
     const num2 = document.getElementById('num2');
     num2.innerHTML = 'I know how to access / select elements in the DOM. 👊';
-    num2.style.backgroundColor = ("yellow"); // Gold
+    num2.style.backgroundColor = ("yellow");
     num2.style.color = ("black");
   
     const num3 = document.getElementById('num3');
@@ -19,11 +19,11 @@
   
     const num4 = document.getElementById('num4');
     num4.innerHTML = 'I know how the styling properties differ in JS from CSS 👍';
-    num4.style.backgroundColor = ("purple"); // Purple
+    num4.style.backgroundColor = ("purple");
   
     const num5 = document.getElementById('num5');
     num5.innerHTML = 'Yes I did it!! I manipulated the DOM and styled my elements 😍😍😍😍';
-    num5.style.backgroundColor = ("Black"); // Black
+    num5.style.backgroundColor = ("Black");
     num5.style.color = ("white");
   
   
